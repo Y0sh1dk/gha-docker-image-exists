1177a72d00fbaf587910e37a39bff8371b41c891
Yosiah de Koeyer <dev@yosiahdekoeyer.dev>
2023-12-11 20:57:31 +1100

    Add some debug statements

--- main.go
+++ main.go
@@ -23,25 +23,33 @@ func main() {
 	ctx := context.Background()
 	action := githubactions.New()
 
+	action.Debugf("Getting inputs...")
 	config, err := NewFromInputs(action)
 	if err != nil {
 		panic(err)
 	}
-
+	action.Debugf("Got inputs")
 	fmt.Println(config)
 
+	action.Debugf("Getting docker client...")
 	client, err := getDockerClient()
 	if err != nil {
 		panic(err)
 	}
-	defer client.Close()
+	action.Debugf("Got docker client")
 
+	action.Debugf("Getting auth string...")
 	authStr, err := config.GetAuthString()
 	if err != nil {
 		panic(err)
 	}
+	action.Debugf("Got auth string")
 
+	action.Debugf("Checking if image exists...")
 	imageExists := doesImageExist(ctx, client, config.image, authStr)
+	action.Debugf("Checked if image exists")
+
+	client.Close()
 
 	if imageExists {
 		fmt.Println("Image exists")
